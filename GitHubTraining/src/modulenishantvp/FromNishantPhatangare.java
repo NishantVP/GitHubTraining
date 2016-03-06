1cd2089cdade429f04210ae9298971ac4972a6c1
Nishant Phatangare <nishantvphatangare@gmail.com>
2016-03-06 02:28:21 -0800

    From NishantPhatangare Account

---
+++ GitHubTraining/src/modulenishantvp/FromNishantPhatangare.java
@@ -0,0 +1,12 @@
+package modulenishantvp;
+
+public class FromNishantPhatangare {
+
+	// I have created this class in new eclipse !
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
